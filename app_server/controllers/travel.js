58fb6783b57792a7bd5ab7c5406bb762ac91839e
Ibrahim <ibrabahlawan9@gmail.com>
2024-09-19 18:58:31 +0300

    Module 3 baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,11 @@
+var fs = require('fs');
+var path = require('path');
+
+const filePath = path.join(__dirname, '..', '..', 'data', 'trips.json');
+var trips = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
 const travelList = (req, res) => {
-    res.render('travel', { title: 'Travel' });
+    res.render('travel', { title: 'Travel Getaways', trips});
 };
 
 module.exports = {
